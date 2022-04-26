ba31c367933a32136a0399ea58e22d76311a0fb1
ocralo <ocralo@gmail.com>
2022-04-25 23:55:16 -0500

    feat: add eslint vite and correction lint

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'https://ocralo.github.io/gha-test/',
-  plugins: [react()],
+  plugins: [react(), eslintPlugin()],
 });
