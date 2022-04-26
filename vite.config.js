a8778f79af4caacc9165dc3f43cdb182bf55e6e3
ocralo <ocralo@gmail.com>
2022-04-25 23:17:56 -0500

    fix: correction base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://ocralo.github.io/',
-  plugins: [react()]
-})
+  base: "https://ocralo.github.io/gha-test/",
+  plugins: [react()],
+});
